e47e6f3833fbd2f17ba6d9cdcabeb1991aaee9c3
Neprine <pjonathanvd@gmail.com>
2017-10-17 21:35:34 +0200

    Romput

--- src/holamon.java
+++ src/holamon.java
@@ -1,5 +1,8 @@
-public class holamon {
+public class Classe {
     public static void main(String[] args) {
-        System.out.println("Hola mon");
+        printHola();
+    }
+    public static void printHola(){
+        System.out.println("Hola món!");
     }
 }
