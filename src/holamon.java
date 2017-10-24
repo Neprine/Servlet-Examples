7a9424550fa752f2f596f9648aa9be16e63e8915
Neprine <pjonathanvd@gmail.com>
2017-10-24 18:43:13 +0200

    Cridada

--- src/holamon.java
+++ src/holamon.java
@@ -2,4 +2,10 @@ public class holamon {
     public static void main(String[] args) {
         System.out.println("Hola mon");
     }
+
+    static void printHola()
+
+    {
+        System.out.println("Hola mon");
+    }
 }
